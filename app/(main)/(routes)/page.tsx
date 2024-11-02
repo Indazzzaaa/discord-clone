84367ff1d18f8981e90226d5a259d221e60dfef6
Sumant Patel <patelsumant19@gmail.com>
2024-11-02 12:10:46 +0530

    Added : dark and light theme support using next-them and shadcn

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,9 +1,11 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <p className="text-3xl font-bold text-indigo-500">
+    <div className="text-3xl font-bold text-indigo-500">
       <UserButton />
-    </p>
+      <ModeToggle />
+    </div>
   );
 }
